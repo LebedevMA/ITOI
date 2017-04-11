07324b60027c0afd6c38757f302424bb5c83b940
LebedevMA <m.lebedev.94@mail.ru>
2017-04-11 12:40:40 +0700

    исправил 4 лабу

--- lab4itoi/lab1itoi/descriptor.cpp
+++ lab4itoi/lab1itoi/descriptor.cpp
@@ -10,27 +10,37 @@ descriptor::descriptor()
 
 descriptor::descriptor(descriptor &a)
 {
-	V = new double[a.size];
 	size = a.size;
-	for (int i = 0;i < a.size;i++) {
-		V[i] = a.V[i];
+	if (size > 0) {
+		V = new Gradient[a.size];
+		for (int i = 0;i < a.size;i++) {
+			V[i] = a.V[i];
+		}
+	}
+	else {
+		V = nullptr;
 	}
 }
 
 void descriptor::operator=(descriptor &a)
 {
-	V = new double[a.size];
 	size = a.size;
-	for (int i = 0;i < a.size;i++) {
-		V[i] = a.V[i];
+	if (size > 0) {
+		V = new Gradient[a.size];
+		for (int i = 0;i < a.size;i++) {
+			V[i] = a.V[i];
+		}
+	}
+	else {
+		V = nullptr;
 	}
 }
 
-std::unique_ptr<descriptor> descriptor::FromPoint(const image &Gx, const image &Gy, const interest_points::point &pt, const int R, const int N) {
-	std::unique_ptr<descriptor> res = std::make_unique<descriptor>();
-	res->V = new double[N];
+std::unique_ptr<descriptor> descriptor::FromPoint(const image &Gx, const image &Gy, const interest_points::point &pt, const int R, const int N){
+	auto res = std::make_unique<descriptor>();
+	res->V = new Gradient[N];
+	res->size = N;
 
-	
 	double max = 0;
 
 	for (int i = -R+1;i < R;i++) {
@@ -41,23 +51,26 @@ std::unique_ptr<descriptor> descriptor::FromPoint(const image &Gx, const image &
 			double y1 = Gy.getPixel(x, y);
 			double phi = atan(y1 / x1);
 			double r1 = sqrt(x1*x1 + y1*y1);
-			int idx = (1.571+phi)*N / 3.142;
-			res->V[idx] += r1;
+			int idx = (1.571+phi)*8 / 3.142;
+			int idx2 = N*((R+i) * 2 * R + (R+j)) / (4 * R*R);
+			res->V[idx2].V[idx] += r1;
 			if (r1 > max) max = r1;
 		}
 	}
 	if (max > 0) {
 		for (int i = 0;i < N;i++) {
-			res->V[i] /= max;
+			for (int j = 0;j < 8;j++) {
+				res->V[i].V[j] /= max;
+			}
 		}
 	}
 	return res;
 }
 
-double descriptor::Distance(const std::unique_ptr<descriptor> &a, const std::unique_ptr<descriptor> &b) {
+double descriptor::Distance(const descriptor &a, const descriptor &b) {
 	double res = 0;
-	for (int i = 0;i < a->size && i < b->size;i++) {
-		double d = a->V[i] - b->V[i];
+	for (int i = 0;i < a.size && i < b.size;i++) {
+		double d = Gradient::Distance(a.V[i], b.V[i]);
 		res += d*d;
 	}
 	return res;
@@ -74,8 +87,8 @@ std::unique_ptr<descriptor::line[]> descriptor::Connect(const interest_points & 
 	auto Gy2 = grad2.convolution(kernel::SobelKy());
 
 	for (int i = 0;i < N;i++) {
-		D1[i] = descriptor::FromPoint(*Gx1, *Gy1, IP.getPoint(i), R, R);
-		D2[i] = descriptor::FromPoint(*Gx2, *Gy2, IP2.getPoint(i), R, R);
+		D1[i] = descriptor::FromPoint(*Gx1, *Gy1, IP.getPoint(i), R, R/2);
+		D2[i] = descriptor::FromPoint(*Gx2, *Gy2, IP2.getPoint(i), R, R/2);
 	}
 
 	auto lines = std::make_unique < line[]>(N*N);
@@ -84,7 +97,7 @@ std::unique_ptr<descriptor::line[]> descriptor::Connect(const interest_points & 
 		interest_points::point a = IP.getPoint(i);
 		for (int j = 0;j < N;j++) {
 			interest_points::point b = IP2.getPoint(j);
-			double d = descriptor::Distance(D1[i], D2[j]);
+			double d = descriptor::Distance(*D1[i], *D2[j]);
 			lines[i*N + j] = line(i, j, d, a, b);
 		}
 	}
--- lab4itoi/lab1itoi/descriptor.h
+++ lab4itoi/lab1itoi/descriptor.h
@@ -5,7 +5,30 @@
 
 class descriptor
 {
-	double *V;
+public:
+	struct Gradient {
+		double V[8];
+		Gradient() {
+			for (int i = 0;i < 8;i++) {
+				V[i] = 0;
+			}
+		}
+		Gradient(const Gradient &a) {
+			for (int i = 0;i < 8;i++) {
+				V[i] = a.V[i];
+			}
+		}
+		static double Distance(const Gradient &a, const Gradient &b) {
+			double res = 0;
+			for (int i = 0;i < 8;i++) {
+				double d = a.V[i] - b.V[i];
+				res += d*d;
+			}
+			return sqrt(res);
+		}
+	};
+private:
+	Gradient *V;
 	int size;
 
 public:
@@ -33,12 +56,12 @@ public:
 	};
 
 public:
-	double getElement(const int index) { return V[index]; };
+	Gradient getElement(const int index) { return V[index]; };
 	int getSize() { return size; }
 
 	static std::unique_ptr<descriptor> FromPoint(const image &Gx, const image &Gy, const interest_points::point &pt, const int R, const int N);
 
-	static double Distance(const std::unique_ptr<descriptor> &a, const std::unique_ptr<descriptor> &b);
+	static double Distance(const descriptor &a, const descriptor &b);
 
 	static std::unique_ptr<line[]> Connect(const interest_points &IP, const interest_points &IP2, const image &grad1, const image &grad2, int N, int R);
 
